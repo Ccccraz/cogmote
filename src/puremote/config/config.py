7b0fddadf409a556d48c3ff75479de6b7283da1d
HuYang <hy.ccccr@gmail.com>
2025-03-27 13:26:10 +0800

    feat: :sparkles: remove unused deps and refactor config

--- src/puremote/config/config.py
+++ src/puremote/config/config.py
@@ -5,6 +5,8 @@ import json
 import os
 import appdirs
 
+from puremote.common.logger import logger
+
 
 class Figure(BaseModel):
     nickname: str
@@ -12,7 +14,7 @@ class Figure(BaseModel):
     y_axis: str
 
 
-class Config(BaseModel):
+class ConfigModel(BaseModel):
     """
     Configuration model class for storing various configuration parameters of the application
 
@@ -51,64 +53,56 @@ CONFIG_FILE_PATH = Path(
     )
 )
 
-config: Config | None = None
-
-
-def create_default_config():
-    default_config = Config()
-    try:
-        CONFIG_FILE_PATH.parent.mkdir(parents=True, exist_ok=True)
-
-        with CONFIG_FILE_PATH.open("w") as f:
-            json.dump(default_config.model_dump(), f, indent=4)
-    except IOError:
-        print("Failed to create configuration file")
-
-
-def load_config():
-    global config
-    global CONFIG_FILE_PATH
-
-    # Check if the file exists
-    if not os.path.exists(CONFIG_FILE_PATH):
-        print("Configuration file not found")
-        create_default_config()
-
-    # Check if the file is readable
-    if not os.access(CONFIG_FILE_PATH, os.R_OK):
-        print("No permission to read the configuration file")
-        return
-
-    try:
-        with open(CONFIG_FILE_PATH, "r") as f:
-            config_data = json.load(f)
-
-        # Check if the data is valid
-        config = Config(**config_data)
-    except json.JSONDecodeError:
-        print("Configuration file format error")
-    except ValidationError as e:
-        print(f"Configuration file is invalid: {e}")
-
-
-def get_config() -> Config:
-    if config is None:
-        load_config()
-
-    assert config is not None
-    return config
-
-
-def set_config():
-    if config is not None:
+class Config:
+    def __init__(self):
+        self.config = self._load_config()
+    
+    def _create_default_config(self) -> ConfigModel:
+        config = ConfigModel()
         try:
-            with open(CONFIG_FILE_PATH, "w") as f:
-                json.dump(config.model_dump(), f, indent=4)
-        except IOError:
-            print("Failed to write configuration file")
+            CONFIG_FILE_PATH.parent.mkdir(parents=True, exist_ok=True)
 
+            with CONFIG_FILE_PATH.open("w") as f:
+                json.dump(config.model_dump(), f, indent=4)
+                
+            return config
+        except IOError:
+            logger.error("Failed to create configuration file")
+            pass
+    
+    def _load_config(self) -> ConfigModel:
+        config: ConfigModel | None = None
+        # Check if the file exists
+        if not os.path.exists(CONFIG_FILE_PATH):
+            logger.info("Configuration file not found")
+            logger.info(f"Creating default configuration file at {CONFIG_FILE_PATH}")
+            config = self._create_default_config()
+
+        # Check if the file is readable
+        if not os.access(CONFIG_FILE_PATH, os.R_OK):
+            logger.warning("No permission to read the configuration file")
+            return
+
+        try:
+            with open(CONFIG_FILE_PATH, "r") as f:
+                config_data = json.load(f)
+
+            # Check if the data is valid
+            config = ConfigModel(**config_data)
+            return config
+        except json.JSONDecodeError:
+            logger.error("Configuration file format error")
+        except ValidationError as e:
+            logger.error(f"Configuration file is invalid: {e}")
+    
+    def save(self):
+        if self.config is not None:
+            try:
+                with open(CONFIG_FILE_PATH, "w") as f:
+                    json.dump(self.config.model_dump(), f, indent=4)
+            except IOError:
+                logger.error("Failed to write configuration file")
 
 if __name__ == "__main__":
-    load_config()
-
-    print(get_config().model_dump_json(indent=4))
+    config = Config()
+    logger.info(config.get_config().model_dump_json(indent=4))
