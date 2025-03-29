91698c4e4bdfeac6a5746b2ebcc4096a594a4167
HuYang <hy.ccccr@gmail.com>
2025-03-30 04:49:09 +0800

    feat: Refactor config and TrialDataStore with dataclass and enhancements

--- src/puremote/config/config.py
+++ src/puremote/config/config.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass, field
 from pydantic import BaseModel, ValidationError
 from pathlib import Path
 
@@ -8,10 +9,21 @@ import appdirs
 from puremote.common.logger import logger
 
 
-class Figure(BaseModel):
+@dataclass(eq=True)
+class FigureConfig:
     nickname: str
     x_axis: str
     y_axis: str
+    figure_type: str
+
+
+@dataclass
+class TrialDataConfig:
+    nickname: str
+    address: str
+    mode: str = "sse"
+    data_labels: list[str] = field(default_factory=list)
+    figures: list[FigureConfig] = field(default_factory=list)
 
 
 class ConfigModel(BaseModel):
@@ -27,23 +39,10 @@ class ConfigModel(BaseModel):
         video_monitor_backend (List[str]): List of available video monitoring backends
             - every string in the list represents an available video monitoring backend
             - default value: `["vlc", "opengl"]`。
-
-        trial_data_source (List[Dict[str, str]]): trial data source configuration
-            - every dictionary in the list represents a trial data source
-            - structure of the dictionary:
-                - key (str): nickname of the data source
-                - key (str): data source path or URL
-            _ default value: `[]`
-
-        trial_data_mode (List[str]): request mode for trial data
-            - every item in the list represents an available request mode for trial data
-            - default value: `["polling", "sse"]`
     """
 
     video_source: dict[str, str] = {}
-    trial_data_source: list[dict[str, str]] = []
-    trial_data_mode: list[str] = ["polling", "sse"]
-    figure: list[Figure] = []
+    trial_data: list[TrialDataConfig] = []
 
 
 APP_NAME = "puremote"
@@ -58,6 +57,14 @@ class Configure:
     def __init__(self):
         self.config = self._load_config()
 
+        self.trial_data_address = {
+            trial_data.address: trial_data for trial_data in self.config.trial_data
+        }
+
+        self.trial_data_nickname = {
+            trial_data.nickname: trial_data for trial_data in self.config.trial_data
+        }
+
     def _create_default_config(self) -> ConfigModel:
         config = ConfigModel()
         try:
@@ -69,6 +76,8 @@ class Configure:
             return config
         except IOError:
             logger.error("Failed to create configuration file")
+        finally:
+            return config
 
     def _load_config(self) -> ConfigModel:
         # Check if the file exists
@@ -87,18 +96,49 @@ class Configure:
 
         try:
             with open(CONFIG_FILE_PATH, "r") as f:
+                logger.info(f"Loading configuration file from {CONFIG_FILE_PATH}")
                 return ConfigModel(**json.load(f))
         except json.JSONDecodeError:
             logger.error("Configuration file format error")
+            return self._create_default_config()
         except ValidationError as e:
             logger.error(f"Configuration file is invalid: {e}")
+            return self._create_default_config()
 
-    def save(self):
+    def _save(self):
         try:
             with open(CONFIG_FILE_PATH, "w") as f:
                 json.dump(self.config.model_dump(), f, indent=4)
         except IOError:
             logger.error("Failed to write configuration file")
+
+    def add_trial_data(self, trial_data: TrialDataConfig) -> None:
+        if trial_data.address in self.trial_data_address:
+            logger.warning(
+                f"Trial data with address {trial_data.address} already exists"
+            )
+            return
+
+        self.config.trial_data.append(trial_data)
+
+        self.trial_data_address[trial_data.address] = trial_data
+        self.trial_data_nickname[trial_data.nickname] = trial_data
+
+        self._save()
+
+    def add_figure(self, address: str, figure: FigureConfig) -> None:
+        if address not in self.trial_data_address:
+            logger.warning(f"Trial data with address {address} does not exist")
+            return
+
+        figures = self.trial_data_address[address].figures
+
+        if figure in figures:
+            logger.warning(f"Figure {figure.nickname} already exists")
+            return
+
+        figures.append(figure)
+        self._save()
 
 
 config_store = Configure()
