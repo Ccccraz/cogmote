84a78cf869157915bfe57a6a42ef08d16eb2f96f
HuYang <hy.ccccr@gmail.com>
2025-08-14 10:32:30 +0200

    feat: :tada: switch from pyside6 to tauri2

---
+++ src/router/index.ts
@@ -0,0 +1,46 @@
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "@/views/HomeView.vue";
+import DataQuickJudgeView from "@/views/DataQuickJudgeView.vue";
+import VideoMonitorView from "@/views/VideoMonitorView.vue";
+import DeviceHomeView from "@/views/DeviceHomeView.vue";
+import Dashboardview from "@/views/Dashboardview.vue"
+import Deviceview from "@/views/Deviceview.vue";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: HomeView,
+    },
+    {
+      path: "/device",
+      name: "device",
+      component: DeviceHomeView,
+      children: [
+        {
+          path: ":address",
+          component: Dashboardview,
+        },
+      ],
+    },
+    {
+      path: "/quick-judge",
+      name: "quickJudge",
+      component: DataQuickJudgeView,
+    },
+    {
+      path: "/monitor",
+      name: "monitor",
+      component: VideoMonitorView,
+    },
+    // {
+    //   path: "/test/:address",
+    //   name: "test",
+    //   component: Deviceview,
+    // },
+  ],
+});
+
+export default router;
---
+++ src/views/ExperimentsView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import DeviceList from '@/components/DeviceList.vue';
+
+</script>
+
+<template>
+    <div class=" flex-1">
+        <DeviceList />
+    </div>
+</template>
