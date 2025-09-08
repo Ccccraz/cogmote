a1b757ea1c27e835b69f979a76c5a40cabea9e5c
HuYang <hy.ccccr@gmail.com>
2025-09-08 03:33:45 +0200

    fix: :bug: Fixed the issue of nested route navigation

--- src/renderer/src/views/VideoMonitorView.vue
+++ src/renderer/src/views/VideoMonitorView.vue
@@ -11,7 +11,7 @@ const deviceStore = useDeviceStore()
     <div class="grid grid-cols-3 justify-items-center p-4 gap-4">
       <div
         v-for="[address] in deviceStore.devices"
-        :key="address"
+        :key="`video-monitor-view-${address}`"
         class="w-full aspect-16/9 flex flex-col gap-4 p-4 bg-muted/50 border rounded-xl"
       >
         <Badge>{{ deviceStore.getDevice(`${address}`)?.device.hostname }}</Badge>
